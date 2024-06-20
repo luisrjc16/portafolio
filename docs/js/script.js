ccd2bb65c2cfc7a30bd88182532f139d3cedddfc
Luis Jaime Calva <lrjc2508@gmail.com>
2024-06-20 16:58:39 -0600

    Se añade carpeta docs

---
+++ docs/js/script.js
@@ -0,0 +1,52 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+	menuIcon.classList.toggle('bx-x'); 
+	navbar.classList.toggle('active');
+};
+
+/* Secciones */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+	sections.forEach(sec => {
+		let top = window.scrollY;
+		let offset = sec.offsetTop - 150;
+		let height = sec.offsetHeight;
+		let id = sec.getAttribute('id');
+		
+		if(top >= offset && top < offset + height) {
+			navLinks.forEach(links => {
+				links.classList.remove('active');
+				document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+			});
+		};
+	});
+	
+	/*Resposive Menu icon*/
+	/*remove menu icon navbar when click navbar Link (scroll)*/
+	menuIcon.classList.remove('bx-x');
+	navbar.classList.remove('active');
+};
+
+/*
+	Transiciones
+*/
+
+window.addEventListener('scroll', reveal);
+function reveal(){
+	var reveals = document.querySelectorAll('.reveal');
+	for(var i=0; i < reveals.length; i++){
+		var windowheight = window.innerHeight;
+		var revealtop = reveals[i].getBoundingClientRect().top;
+		var revealpoint = 150;
+		
+		if(revealtop < windowheight - revealpoint) { 
+			reveals[i].classList.add('active');
+		}else{
+			reveals[i].classList.remove('active');
+		}
+	}
+}
